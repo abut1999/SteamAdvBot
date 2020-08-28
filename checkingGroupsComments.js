15a52dbe8869634aca55114ec35cad887fc1aeb9
abut1999 <62015165+abut1999@users.noreply.github.com>
2020-08-28 16:47:47 +0200

    second commit

--- checkingGroupsComments.js
+++ checkingGroupsComments.js
@@ -4,30 +4,39 @@ require('dotenv').config();
 
 const postingGroupCommentModule = {};
 
-getGeryRecentComments=async() => {
-  const requestHedaer = {
-    method: 'GET',
-    uri: process.env.GERY_URI,
-    headers: {
-      Cookie: process.env.CONN_STRING
-    }
-  }
-
-  const recentCommentsHTML = await request(requestHedaer);
-  const $ = cheerio.load(recentCommentsHTML);
-
+function checkingGroupComments(requestedHTML) {
+  const $ = cheerio.load(requestedHTML);
   const recentCommentsAuthors = [];
-  $('#commentthread_Clan_103582791462433695_posts > div').each((_i, e) => {
+  $('.commentthread_comment_container .commentthread_comments .commentthread_comment.responsive_body_text').each((_i, e) => {
     const eachComment = cheerio.load(e);
     const authorID = eachComment('div.commentthread_comment_content > div.commentthread_comment_author > a').attr('href')
     recentCommentsAuthors.push(authorID);
   });
-  
-  checkingOutcome = ((recentCommentsAuthors.includes(process.env.CREATOR)) === true) ? "Already in the top page" : "import bumping module function";
+  console.log(recentCommentsAuthors)
+  return recentCommentsAuthors;
+}
+
+function requestHeader(requestUri) {
+  const requestHeader = {
+    method: 'GET',
+    uri: requestUri,
+    headers: {
+      Cookie: process.env.CONN_STRING
+    }
+  }
+  return requestHeader;
+}
+
+async function requestRecentComments(targetUri) {
+  const recentCommentsHTML = await request(requestHeader(targetUri));
+  const checkingOutcome = checkingGroupComments(recentCommentsHTML)
+  const returner = ((checkingOutcome.includes(process.env.CREATOR)) === true) ? "Already in the top page" : "import bumping module function";
+  console.log(returner);
 }
 
 postingGroupCommentModule.groupCommentsChecker=async() => {
-  await getGeryRecentComments();
+  await requestRecentComments(process.env.GERY_URI);
+  await requestRecentComments(process.env.ITRADERS_URI)
 }
 
 module.exports = postingGroupCommentModule;
