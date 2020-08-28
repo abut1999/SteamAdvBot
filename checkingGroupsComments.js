458c75974ffe2077f7bd2cd8d1fa64a785d1d5fb
abut1999 <62015165+abut1999@users.noreply.github.com>
2020-08-28 17:47:59 +0200

    comments

--- checkingGroupsComments.js
+++ checkingGroupsComments.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const postingGroupCommentModule = {};
 
+//Global reusable header for different request
 function requestHeader(requestUri) {
   const requestHeader = {
     method: 'GET',
@@ -15,21 +16,28 @@ function requestHeader(requestUri) {
   return requestHeader;
 }
 
+//Global reusable function to check each group comments, targeted URL has to be passed
 function checkingGroupComments(requestedHTML) {
   const $ = cheerio.load(requestedHTML);
   const recentCommentsAuthors = [];
+  //Go through each comment and obtain each author ID, store it in array
   $('.commentthread_comment_container .commentthread_comments .commentthread_comment.responsive_body_text').each((_i, e) => {
     const eachComment = cheerio.load(e);
     const authorID = eachComment('div.commentthread_comment_content > div.commentthread_comment_author > a').attr('href')
     recentCommentsAuthors.push(authorID);
   });
   console.log(recentCommentsAuthors)
+  //Returning array of stored authors ID
   return recentCommentsAuthors;
 }
 
+//Request function, to call global functions
 async function requestRecentComments(targetUri) {
+  //Request the html for targeted group comments
   const recentCommentsHTML = await request(requestHeader(targetUri));
+  //Checking outcome gets the returned array of authors IDs
   const checkingOutcome = checkingGroupComments(recentCommentsHTML)
+  //Check if any of ids in returned array matches our ID, if not call the bumping function
   const returner = ((checkingOutcome.includes(process.env.CREATOR)) === true) ? "Already in the top page" : "import bumping module function";
   console.log(returner);
 }
--- checkingPostsInDiscussions.js
+++ checkingPostsInDiscussions.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const checkingDiscussionsPostsModule = {};
 
+//Global reusable header for each request
 function requestHeader(requestUri) {
   const requestHeader = {
     method: 'GET',
@@ -15,15 +16,21 @@ function requestHeader(requestUri) {
   return requestHeader;
 }
 
+//Check if our discussion id can be grabbed in top discussions
 function checkingMyDiscussionPosition(requestedHTML) {
   const $ = cheerio.load(requestedHTML);
   const attemptedSelectUri = $('#forum_General_32912287_3960307437826307489 > a').attr('href');
+  //Returns either our id or undefined
   return attemptedSelectUri;
 }
 
+//Call global functions to initiate requests
 async function requestRecentDiscussions(targetUri, bumpingUri) {
+  //Request targeted group recent discussions
   const recentDiscussionsHTML = await request(requestHeader(targetUri));
+  //Receive the return of checking outcome (id or undefined)
   const checkingOutcome = checkingMyDiscussionPosition(recentDiscussionsHTML)
+  //Call the bumping module if returned undefined, else do nothing
   if (checkingOutcome === undefined) {
     console.log('export bumping module');
   } else {
